66e96e3ad62f464f82de9b5f9d6efd0ee137b156
Ross Masters <ross@rossmasters.com>
2025-01-19 01:56:45 +0000

    fix: Expose build_fastapi_app from faststack.apps for convenience

--- faststack/apps/__init__.py
+++ faststack/apps/__init__.py
@@ -1,4 +1,5 @@
+from .fastapi_app import build_fastapi_app
 from .ioc import FaststackContainer
 from .settings import FaststackSettings
 
-__all__ = ["FaststackContainer", "FaststackSettings"]
+__all__ = ["FaststackContainer", "FaststackSettings", "build_fastapi_app"]
